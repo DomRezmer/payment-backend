b4786f07323be32f5e13ecfb84f339bdf62caaf3
DomRezmer <dom.rezmer@gmail.com>
2023-09-08 13:24:41 +0200

    springboot init

---
+++ src/main/java/dev/kbe/bezahlungsbackend/BezahlungsBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.kbe.bezahlungsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezahlungsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezahlungsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kbe/bezahlungsbackend/BezahlungsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kbe.bezahlungsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezahlungsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
